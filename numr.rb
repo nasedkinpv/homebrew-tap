4f4b6e06690509d416f052570e0e83011db6ae71
nasedkinpv <>
2025-11-29 23:47:02 +0000

    Update numr to 0.1.6

--- numr.rb
+++ numr.rb
@@ -2,7 +2,7 @@ class Numr < Formula
   desc "Text calculator for natural language expressions with a vim-style TUI"
   homepage "https://github.com/nasedkinpv/numr"
   url "https://github.com/nasedkinpv/numr/archive/v0.1.6.tar.gz"
-  sha256 "0e2097a9022c94205c473512172c8eed3450b6bdd025c24d1c8193480da73ca9"
+  sha256 "706c48e191c67a9bf116078e0697c46aac238119b09c7875916106a681f13374"
   license "MIT"
 
   depends_on "rust" => :build

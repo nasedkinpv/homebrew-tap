67f94afdb3f77d2cfdb14104f0630747d135ae3e
nasedkinpv <>
2026-07-12 23:55:01 +0000

    Update numr to 0.7.0

--- numr.rb
+++ numr.rb
@@ -1,17 +1,17 @@
 class Numr < Formula
   desc "Text calculator for natural language expressions with a vim-style TUI"
   homepage "https://github.com/nasedkinpv/numr"
-  version "0.6.0"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/nasedkinpv/numr/releases/download/v#{version}/numr-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "aaf1e29c3cecc3a2cf346aad944a4af684f31f76c38f736b9696acd7753eddd5"
+      sha256 "d70f56a4abb88047466a6c7653adb058ee63a0b25d4ff831e72485181d4b959f"
     end
     on_intel do
       url "https://github.com/nasedkinpv/numr/releases/download/v#{version}/numr-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "bd2d452b7e03009a363cba02e5e04de5833b23236cefe98db17862049808aff7"
+      sha256 "9e5ff504152aa00d988955b0d2a07f67609b2d9e0938002b66f92d96d4b3827f"
     end
   end
 
